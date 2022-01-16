961e8a52b257f3250c6df5646dc6bc5409f29680
sonledinh <sonledinh@admicro.vn>
2022-01-17 02:13:56 +0700

    update code

---
+++ about.php
@@ -0,0 +1,104 @@
+<?php include 'header.php';?>
+<main>
+	<section class="box-breadcrumbs" style="background: url('images/bread.png') no-repeat center;background-size: cover;">
+		<div class="container">
+			<div class="info-bread">
+				<h2>Giới thiệu</h2>
+				<ul>
+					<li><a href="">Trang chủ</a></li>
+					<li><a href="">Giới thiệu</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<section class="box-pages">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-1">
+					<div class="social-bar">
+						<ul>
+							<li><a href=""><i class="fa fa-facebook"></i></a></li>
+							<li><a href=""><i class="fa fa-twitter"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-8">
+					<div class="content-detail">
+						<div class="info-detail">
+							<p>Bài viết chia sẻ các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của dạng bài nghe tả tranh trong TOEIC Listening Part 1.</p>
+							<h2>Cách tránh bẫy trong từng dạng câu hỏi TOEIC Listening Part 1 – Tả tranh</h2>
+							<h3>Phương pháp làm bài</h3>
+							<h4>Bước 1: Phân tích tranh</h4>
+							<h5>Bước 2: Nghe các đáp án A, B, C, D (mỗi đáp án cách nhau 5s)</h5>
+							<h6>Bước 3: Chọn đáp án. Ngay sau đó tiếp tục nhìn xuống bức tranh tiếp theo và lặp lại bước 1 cho đến khi hết 6 bức tranh.</h6>
+							<ul>
+								<li>Bài viết chia sẻ các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của dạng bài nghe tả tranh trong TOEIC Listening Part 1.</li>
+								<li>Bài viết chia sẻ các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của dạng bài nghe tả tranh trong TOEIC Listening Part 1.</li>
+								<li>Bài viết chia sẻ các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của dạng bài nghe tả tranh trong TOEIC Listening Part 1.</li>
+								<li>Bài viết chia sẻ các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của dạng bài nghe tả tranh trong TOEIC Listening Part 1.</li>
+								<li>Bài viết chia sẻ các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của dạng bài nghe tả tranh trong TOEIC Listening Part 1.</li>
+							</ul>
+							<p>Tiếp tục phần trước của “<a href="">Phương pháp làm bài và cách nghe tránh bẫy trong TOEIC Listening Part 1</a>“, bài viết dưới đây sẽ đưa ra các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của bài nghe tả tranh. </p>
+							<p>Đây là dạng dễ nhất trong bài thi TOEIC Listening nên thí sinh nên cố gắng làm đúng hết 6 câu dạng này thông qua luyện tập và những cách để tránh bẫy trong phần thi này. </p>
+							<p>Các loại tranh xuất hiện trong bài thi TOEIC Listening Part 1: Tả người (một hoặc nhiều người); tả vật và tả cảnh.</p>
+							<p>Tiếp tục phần trước của “<a href="">Phương pháp làm bài và cách nghe tránh bẫy trong TOEIC Listening Part 1</a>“, bài viết dưới đây sẽ đưa ra các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của bài nghe tả tranh. </p>
+							<p>Đây là dạng dễ nhất trong bài thi TOEIC Listening nên thí sinh nên cố gắng làm đúng hết 6 câu dạng này thông qua luyện tập và những cách để tránh bẫy trong phần thi này. </p>
+							<p>Các loại tranh xuất hiện trong bài thi TOEIC Listening Part 1: Tả người (một hoặc nhiều người); tả vật và tả cảnh.</p>
+							<p>Tiếp tục phần trước của “<a href="">Phương pháp làm bài và cách nghe tránh bẫy trong TOEIC Listening Part 1</a>“, bài viết dưới đây sẽ đưa ra các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của bài nghe tả tranh. </p>
+							<p><img src="images/detail.png" alt=""></p>
+							<p>Đây là dạng dễ nhất trong bài thi TOEIC Listening nên thí sinh nên cố gắng làm đúng hết 6 câu dạng này thông qua luyện tập và những cách để tránh bẫy trong phần thi này. </p>
+							<p>Các loại tranh xuất hiện trong bài thi TOEIC Listening Part 1: Tả người (một hoặc nhiều người); tả vật và tả cảnh.</p>
+							<p>Tiếp tục phần trước của “<a href="">Phương pháp làm bài và cách nghe tránh bẫy trong TOEIC Listening Part 1</a>“, bài viết dưới đây sẽ đưa ra các cách nghe tránh bẫy cụ thể, hỗ trợ người học luyện tập và dành điểm tối đa của bài nghe tả tranh. </p>
+							<p>Đây là dạng dễ nhất trong bài thi TOEIC Listening nên thí sinh nên cố gắng làm đúng hết 6 câu dạng này thông qua luyện tập và những cách để tránh bẫy trong phần thi này. </p>
+							<p>Các loại tranh xuất hiện trong bài thi TOEIC Listening Part 1: Tả người (một hoặc nhiều người); tả vật và tả cảnh.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="content-table">
+						<h3>Mục lục</h3>
+						<ul>
+							<li>
+								<a href="" class="active"><span>1.</span>Cách tránh bẫy trong từng dạng câu hỏi TOEIC Listening Part 1 – Tả tranh</a>
+								<ul>
+									<li><a href=""><span>1.1</span>Phương pháp làm bài</a></li>
+									<li><a href=""><span>1.2</span>Nghe tránh bẫy dạng câu hỏi tranh về người</a></li>
+									<li><a href=""><span>1.3</span>Nghe tránh bẫy dạng câu hỏi tranh về đồ vật</a></li>
+								</ul>
+							</li>
+							<li><a href=""><span>2.</span>Kết luận</a></li>
+						</ul>
+					</div>
+					<div class="bar-register">
+						<h3 class="text-center">Đăng ký tư vấn</h3>
+						<div class="frm-regis">
+							<div class="item-regis"><input type="text" placeholder="Họ tên" class="txt_field"></div>
+							<div class="item-regis"><input type="text" placeholder="Email" class="txt_field"></div>
+							<div class="item-regis"><input type="text" placeholder="Điện thoại" class="txt_field"></div>
+							<div class="item-regis"><textarea placeholder="Ghi chú"></textarea></div>
+							<div class="item-regis"><input type="submit" value="Đăng ký" class="btn_field"></div>
+						</div>
+					</div>
+					<div class="bar-support">
+						<h3>Hỗ trợ tư vấn:</h3>
+						<ul>
+							<li>
+								<span>Ms. Trang Trần</span>
+								<label>0932 888 383</label>
+							</li>
+							<li>
+								<span>Ms. Diệu Linh</span>
+								<label>0932 888 324</label>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?>     
+ 
+
+
+
---
+++ lession.php
@@ -0,0 +1,330 @@
+<?php include 'header.php';?>
+<main>
+	<section class="box-breadcrumbs" style="background: url('images/bread.png') no-repeat center;background-size: cover;">
+		<div class="container">
+			<div class="info-bread">
+				<h2>Khóa học</h2>
+				<ul>
+					<li><a href="">Trang chủ</a></li>
+					<li><a href="">Khóa học</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<section class="box-benefit">
+		<div class="container">
+			<div class="content-ben">
+				<div class="row">
+					<div class="col-md-9">
+						<div class="title">
+							<h2>5 lợi ích khi học tại Zilom</h2>
+						</div>
+						<div class="list-ben">
+							<div class="row">
+								<div class="col-md-4">
+									<div class="item-ben">
+										<div class="numb">01</div>
+										<div class="desc">Rút ngắn <span>80% thời gian học và thi</span></div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="item-ben">
+										<div class="numb">02</div>
+										<div class="desc"><span>100%</span> cam kết đạt đầu ra</div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="item-ben">
+										<div class="numb">03</div>
+										<div class="desc">Học cùng <span>chuyên gia</span> luyện thi</div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="item-ben">
+										<div class="numb">04</div>
+										<div class="desc">Hệ thống giáo trình <span>cá nhân hóa</span></div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="item-ben">
+										<div class="numb">05</div>
+										<div class="desc">Kiểm tra <span>năng lực</span> đầu vào</div>
+									</div>
+								</div>
+							</div>
+						</div> 
+					</div>
+					<div class="col-md-3">
+						<div class="bar-right">
+							<div class="bar-register">
+								<h3 class="text-center">Đăng ký tư vấn</h3>
+								<div class="frm-regis">
+									<div class="item-regis"><input type="text" placeholder="Họ tên" class="txt_field"></div>
+									<div class="item-regis"><input type="text" placeholder="Email" class="txt_field"></div>
+									<div class="item-regis"><input type="text" placeholder="Điện thoại" class="txt_field"></div>
+									<div class="item-regis"><textarea placeholder="Ghi chú"></textarea></div>
+									<div class="item-regis"><input type="submit" value="Đăng ký" class="btn_field"></div>
+								</div>
+							</div>
+							<div class="bar-support">
+								<h3>Hỗ trợ tư vấn:</h3>
+								<ul>
+									<li>
+										<span>Ms. Trang Trần</span>
+										<label>0932 888 383</label>
+									</li>
+									<li>
+										<span>Ms. Diệu Linh</span>
+										<label>0932 888 324</label>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="box-commit">
+		<div class="container">
+			<div class="content-commit">
+				<div class="title">
+					<h2 class="text-center">Cam kết chất lượng</h2>
+				</div>
+				<div class="list-commit">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="item-commit">
+								<div class="icon"><img src="images/check.png" class="img-fluid" alt=""></div>
+								<div class="info">
+									Kiểm tra trình độ đầu vào chính xác và miễn phí: ZIM đánh giá chính xác trình độ của học viên trước khi xếp lớp
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="item-commit">
+								<div class="icon"><img src="images/check.png" class="img-fluid" alt=""></div>
+								<div class="info">
+									Học thử trải nghiệm khách quan trước khi đăng ký: Học viên an tâm thử nghiệm trước khi quyết định đăng ký học
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="item-commit">
+								<div class="icon"><img src="images/check.png" class="img-fluid" alt=""></div>
+								<div class="info">
+									Cam kết đảm bảo đầu ra Zero-risk uy tín: Học viên được đào tạo lại miễn phí nếu không đạt đầu ra
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="item-commit">
+								<div class="icon"><img src="images/check.png" class="img-fluid" alt=""></div>
+								<div class="info">
+									Hệ thống học liệu hoàn thiện và phương pháp giảng dạy cá nhân hoá: đảm bảo học viên được trải nghiệm chất lượng chuyên môn tốt nhất
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="item-commit">
+								<div class="icon"><img src="images/check.png" class="img-fluid" alt=""></div>
+								<div class="info">
+									Giảng viên trình độ tối thiểu IELTS 7.5 - 8.5. Giảng viên tại Zilom có trình độ chuyên môn ưu tú và đã tham gia các dự án phát triển nguồn học liệu và phương pháp sư phạm tại Zilom
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="item-commit">
+								<div class="icon"><img src="images/check.png" class="img-fluid" alt=""></div>
+								<div class="info">
+									Hoàn tiền 100% trong 2 tuần đầu nếu không hài lòng.
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="box-subject">
+		<div class="container">
+			<div class="title text-center">
+				<h2>Lịch khai giảng</h2>
+			</div>
+			<div class="search-sub">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="item-search">
+							<label>Từ khóa:</label>
+							<input type="text" placeholder="Tên lớp học, Giáo viên...">
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-search">
+							<label>Khu vực:</label>
+							<select name="" id="">
+								<option value="">Hà Nội</option>
+								<option value="">Hà Nội</option>
+								<option value="">Hà Nội</option>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-search">
+							<label>Trung tâm:</label>
+							<select name="" id="">
+								<option value="">Zilom Thanh Xuân</option>
+								<option value="">Zilom Thanh Xuân</option>
+								<option value="">Zilom Thanh Xuân</option>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-search">
+							<label>Level:</label>
+							<select name="" id="">
+								<option value="">Tất cả</option>
+								<option value="">Tất cả</option>
+								<option value="">Tất cả</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="list-subject">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="item-subject">
+							<div class="avarta"><a href=""><img src="images/sub-1.png" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<h3><a href="">Online IELTS Level 7.5</a></h3>
+								<div class="numb-sec">
+									<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7 3.0625C7 2.94647 6.95391 2.83519 6.87186 2.75314C6.78981 2.67109 6.67853 2.625 6.5625 2.625C6.44647 2.625 6.33519 2.67109 6.25314 2.75314C6.17109 2.83519 6.125 2.94647 6.125 3.0625V7.875C6.12502 7.95212 6.14543 8.02785 6.18415 8.09454C6.22288 8.16123 6.27854 8.2165 6.3455 8.25475L9.408 10.0048C9.5085 10.0591 9.62626 10.0719 9.73611 10.0406C9.84596 10.0092 9.93919 9.93611 9.99587 9.83692C10.0525 9.73774 10.0682 9.62031 10.0394 9.50975C10.0107 9.39919 9.93982 9.30426 9.842 9.24525L7 7.62125V3.0625Z" fill="#4D5FE3"></path>
+									</svg>
+									<span>15 buổi</span>
+								</div>
+								<div class="view-more text-center"><a href="">Chi tiết</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-subject">
+							<div class="avarta"><a href=""><img src="images/sub-2.png" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<h3><a href="">Online IELTS Level 7.5</a></h3>
+								<div class="numb-sec">
+									<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7 3.0625C7 2.94647 6.95391 2.83519 6.87186 2.75314C6.78981 2.67109 6.67853 2.625 6.5625 2.625C6.44647 2.625 6.33519 2.67109 6.25314 2.75314C6.17109 2.83519 6.125 2.94647 6.125 3.0625V7.875C6.12502 7.95212 6.14543 8.02785 6.18415 8.09454C6.22288 8.16123 6.27854 8.2165 6.3455 8.25475L9.408 10.0048C9.5085 10.0591 9.62626 10.0719 9.73611 10.0406C9.84596 10.0092 9.93919 9.93611 9.99587 9.83692C10.0525 9.73774 10.0682 9.62031 10.0394 9.50975C10.0107 9.39919 9.93982 9.30426 9.842 9.24525L7 7.62125V3.0625Z" fill="#4D5FE3"></path>
+									</svg>
+									<span>15 buổi</span>
+								</div>
+								<div class="view-more text-center"><a href="">Chi tiết</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-subject">
+							<div class="avarta"><a href=""><img src="images/sub-1.png" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<h3><a href="">Online IELTS Level 7.5</a></h3>
+								<div class="numb-sec">
+									<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7 3.0625C7 2.94647 6.95391 2.83519 6.87186 2.75314C6.78981 2.67109 6.67853 2.625 6.5625 2.625C6.44647 2.625 6.33519 2.67109 6.25314 2.75314C6.17109 2.83519 6.125 2.94647 6.125 3.0625V7.875C6.12502 7.95212 6.14543 8.02785 6.18415 8.09454C6.22288 8.16123 6.27854 8.2165 6.3455 8.25475L9.408 10.0048C9.5085 10.0591 9.62626 10.0719 9.73611 10.0406C9.84596 10.0092 9.93919 9.93611 9.99587 9.83692C10.0525 9.73774 10.0682 9.62031 10.0394 9.50975C10.0107 9.39919 9.93982 9.30426 9.842 9.24525L7 7.62125V3.0625Z" fill="#4D5FE3"></path>
+									</svg>
+									<span>15 buổi</span>
+								</div>
+								<div class="view-more text-center"><a href="">Chi tiết</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-subject">
+							<div class="avarta"><a href=""><img src="images/sub-2.png" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<h3><a href="">Online IELTS Level 7.5</a></h3>
+								<div class="numb-sec">
+									<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7 3.0625C7 2.94647 6.95391 2.83519 6.87186 2.75314C6.78981 2.67109 6.67853 2.625 6.5625 2.625C6.44647 2.625 6.33519 2.67109 6.25314 2.75314C6.17109 2.83519 6.125 2.94647 6.125 3.0625V7.875C6.12502 7.95212 6.14543 8.02785 6.18415 8.09454C6.22288 8.16123 6.27854 8.2165 6.3455 8.25475L9.408 10.0048C9.5085 10.0591 9.62626 10.0719 9.73611 10.0406C9.84596 10.0092 9.93919 9.93611 9.99587 9.83692C10.0525 9.73774 10.0682 9.62031 10.0394 9.50975C10.0107 9.39919 9.93982 9.30426 9.842 9.24525L7 7.62125V3.0625Z" fill="#4D5FE3"></path>
+									</svg>
+									<span>15 buổi</span>
+								</div>
+								<div class="view-more text-center"><a href="">Chi tiết</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-subject">
+							<div class="avarta"><a href=""><img src="images/sub-1.png" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<h3><a href="">Online IELTS Level 7.5</a></h3>
+								<div class="numb-sec">
+									<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7 3.0625C7 2.94647 6.95391 2.83519 6.87186 2.75314C6.78981 2.67109 6.67853 2.625 6.5625 2.625C6.44647 2.625 6.33519 2.67109 6.25314 2.75314C6.17109 2.83519 6.125 2.94647 6.125 3.0625V7.875C6.12502 7.95212 6.14543 8.02785 6.18415 8.09454C6.22288 8.16123 6.27854 8.2165 6.3455 8.25475L9.408 10.0048C9.5085 10.0591 9.62626 10.0719 9.73611 10.0406C9.84596 10.0092 9.93919 9.93611 9.99587 9.83692C10.0525 9.73774 10.0682 9.62031 10.0394 9.50975C10.0107 9.39919 9.93982 9.30426 9.842 9.24525L7 7.62125V3.0625Z" fill="#4D5FE3"></path>
+									</svg>
+									<span>15 buổi</span>
+								</div>
+								<div class="view-more text-center"><a href="">Chi tiết</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-subject">
+							<div class="avarta"><a href=""><img src="images/sub-2.png" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<h3><a href="">Online IELTS Level 7.5</a></h3>
+								<div class="numb-sec">
+									<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7 3.0625C7 2.94647 6.95391 2.83519 6.87186 2.75314C6.78981 2.67109 6.67853 2.625 6.5625 2.625C6.44647 2.625 6.33519 2.67109 6.25314 2.75314C6.17109 2.83519 6.125 2.94647 6.125 3.0625V7.875C6.12502 7.95212 6.14543 8.02785 6.18415 8.09454C6.22288 8.16123 6.27854 8.2165 6.3455 8.25475L9.408 10.0048C9.5085 10.0591 9.62626 10.0719 9.73611 10.0406C9.84596 10.0092 9.93919 9.93611 9.99587 9.83692C10.0525 9.73774 10.0682 9.62031 10.0394 9.50975C10.0107 9.39919 9.93982 9.30426 9.842 9.24525L7 7.62125V3.0625Z" fill="#4D5FE3"></path>
+									</svg>
+									<span>15 buổi</span>
+								</div>
+								<div class="view-more text-center"><a href="">Chi tiết</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-subject">
+							<div class="avarta"><a href=""><img src="images/sub-1.png" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<h3><a href="">Online IELTS Level 7.5</a></h3>
+								<div class="numb-sec">
+									<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7 3.0625C7 2.94647 6.95391 2.83519 6.87186 2.75314C6.78981 2.67109 6.67853 2.625 6.5625 2.625C6.44647 2.625 6.33519 2.67109 6.25314 2.75314C6.17109 2.83519 6.125 2.94647 6.125 3.0625V7.875C6.12502 7.95212 6.14543 8.02785 6.18415 8.09454C6.22288 8.16123 6.27854 8.2165 6.3455 8.25475L9.408 10.0048C9.5085 10.0591 9.62626 10.0719 9.73611 10.0406C9.84596 10.0092 9.93919 9.93611 9.99587 9.83692C10.0525 9.73774 10.0682 9.62031 10.0394 9.50975C10.0107 9.39919 9.93982 9.30426 9.842 9.24525L7 7.62125V3.0625Z" fill="#4D5FE3"></path>
+									</svg>
+									<span>15 buổi</span>
+								</div>
+								<div class="view-more text-center"><a href="">Chi tiết</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="item-subject">
+							<div class="avarta"><a href=""><img src="images/sub-2.png" class="img-fluid w-100" alt=""></a></div>
+							<div class="info">
+								<h3><a href="">Online IELTS Level 7.5</a></h3>
+								<div class="numb-sec">
+									<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M14 7C14 8.85652 13.2625 10.637 11.9497 11.9497C10.637 13.2625 8.85652 14 7 14C5.14348 14 3.36301 13.2625 2.05025 11.9497C0.737498 10.637 0 8.85652 0 7C0 5.14348 0.737498 3.36301 2.05025 2.05025C3.36301 0.737498 5.14348 0 7 0C8.85652 0 10.637 0.737498 11.9497 2.05025C13.2625 3.36301 14 5.14348 14 7ZM7 3.0625C7 2.94647 6.95391 2.83519 6.87186 2.75314C6.78981 2.67109 6.67853 2.625 6.5625 2.625C6.44647 2.625 6.33519 2.67109 6.25314 2.75314C6.17109 2.83519 6.125 2.94647 6.125 3.0625V7.875C6.12502 7.95212 6.14543 8.02785 6.18415 8.09454C6.22288 8.16123 6.27854 8.2165 6.3455 8.25475L9.408 10.0048C9.5085 10.0591 9.62626 10.0719 9.73611 10.0406C9.84596 10.0092 9.93919 9.93611 9.99587 9.83692C10.0525 9.73774 10.0682 9.62031 10.0394 9.50975C10.0107 9.39919 9.93982 9.30426 9.842 9.24525L7 7.62125V3.0625Z" fill="#4D5FE3"></path>
+									</svg>
+									<span>15 buổi</span>
+								</div>
+								<div class="view-more text-center"><a href="">Chi tiết</a></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="view-all text-center">
+							<a href="">Xem tất cả</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?>     
+ 
+
+
+
