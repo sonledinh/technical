bb18b4c75fe716959e45d8e47e8da3c3685217e4
SonML <ledinhson209@gmail.com>
2022-04-25 10:23:34 +0700

    update

--- footer.php
+++ footer.php
@@ -1,110 +1,34 @@
-<section class="box-quote text-center" style="background: url('images/bg-quote.png') no-repeat center; background-size: cover;">
-	<div class="container">
-		<div class="desc">
-			Học hỏi từ các chuyên gia của chúng tôi và nâng <br> cao kỹ năng của bạn
-		</div>
-	</div> 
-</section>
 <footer>
 	<div class="fter-top">
 		<div class="container">
-			<div class="fter-logo">
-				<div class="row align-items-center">
-					<div class="col-md-6">
-						<div class="logo"><a href=""><img src="images/logo-light.png" class="img-fluid" alt=""></a></div>
-					</div>
-					<div class="col-md-6">
-						<div class="social">
-							<ul>
-								<li><a href=""><i class="fa fa-facebook"></i></a></li>
-								<li><a href=""><i class="fa fa-youtube-play"></i></a></li>
-								<li><a href=""><i class="fa fa-twitter"></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="fter-bot">
-		<div class="container">
 			<div class="row">
-				<div class="col-md-3">
-					<div class="item-fter">
-						<h3>Liên hệ</h3>
-						<div class="info-place">
-							<ul>
-								<li>
-									<div class="i-plc">
-										<div class="icon"><img src="images/sc-1.png" class="img-fluid" alt=""></div>
-										<div class="info">
-											<label>Hotline</label>
-											<p>0928 737 717</p>
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="i-plc">
-										<div class="icon"><img src="images/sc-2.png" class="img-fluid" alt=""></div>
-										<div class="info">
-											<label>Email</label>
-											<p>demo@gmail.com.vn</p>
-										</div>
-									</div>
-								</li>
-								<li>
-									<div class="i-plc">
-										<div class="icon"><img src="images/sc-3.png" class="img-fluid" alt=""></div>
-										<div class="info">
-											<label>Văn phòng</label>
-											<p>65 Yên Lãng, Phường Trung Liệt, Quận Đống Đa</p>
-										</div>
-									</div>
-								</li>
-							</ul>
-						</div>
+				<div class="col-md-6">
+					<div class="txt-fter">
+						<h4>CÔNG TY CỔ PHẦN 9PAY</h4>
+						<ul>
+							<li>MST: 0108425897</li>
+							<li>Giấy phép cung ứng dịch vụ TGTT : Số 60/GP-NHNN</li>
+							<li>A : Số 34 Nguyễn Khánh Toàn, Hà Nội, Việt Nam.</li>
+							<li>P : 1900 88 68 32</li>
+							<li>E : hotro@9pay.vn</li>
+						</ul>
+					</div>
+					<div class="social">
+						<ul>
+							<li><a href="" target="_blank"><img src="images/sc-1.png" class="img-fluid" alt=""></a></li>
+							<li><a href="" target="_blank"><img src="images/sc-2.png" class="img-fluid" alt=""></a></li>
+							<li><a href="" target="_blank"><img src="images/sc-3.png" class="img-fluid" alt=""></a></li>
+						</ul>
 					</div>
 				</div>
-				<div class="col-md-3">
-					<div class="item-fter">
-						<h3>Về chúng tôi</h3>
-						<div class="link-place">
-							<ul>
-								<li><a href="">Giới thiệu</a></li>
-								<li><a href="">Khóa học</a></li>
-								<li><a href="">Giáo viên</a></li>
-								<li><a href="">Tuyển dụng</a></li>
-								<li><a href="">Sách</a></li>
-								<li><a href="">Liên hệ</a></li> 
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="item-fter">
-						<h3>Chăm sóc khách hàng</h3>
-						<div class="link-place">
-							<ul>
-								<li><a href="">Chính sách bảo mật</a></li>
-								<li><a href="">Điều khoản sử dụng</a></li>
-								<li><a href="">Chính sách liên kết</a></li>
-								<li><a href="">Chính sách thanh toán</a></li>
-								<li><a href="">Quy chế hoạt động</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-3">
-					<div class="item-fter">
-						<h3>Khám phá</h3>
-						<div class="frm-fter">
-							<p>Nhận thông tin hằng ngày</p>
-							<div class="form-ct">
-								<input type="text" placeholder="Email của bạn">
-								<img src="images/mail.png" class="img-fluid" alt="">
-							</div>
-							<div class="dcma"><a href=""><img src="images/dcma.png" class="img-fluid" alt=""></a></div>
-						</div>
+				<div class="col-md-6">
+					<div class="part-fter">
+						<ul>
+							<li><a href=""><img src="images/fter-1.png" class="img-fluid" alt=""></a></li>
+							<li><a href=""><img src="images/fter-2.png" class="img-fluid" alt=""></a></li>
+							<li><a href=""><img src="images/fter-3.png" class="img-fluid" alt=""></a></li>
+							<li><a href=""><img src="images/fter-4.png" class="img-fluid" alt=""></a></li>
+						</ul>
 					</div>
 				</div>
 			</div>
@@ -114,6 +38,7 @@
 <!--Link js--> 
 <script type="text/javascript" src="js/lib/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/lib/slick.min.js"></script>
+<script type="text/javascript" src="js/aos.min.js"></script>
 <script type="text/javascript" src="js/lib/jquery.mmenu.all.js"></script>
 <script type="text/javascript" src="js/private.js"></script> 
 </body>
